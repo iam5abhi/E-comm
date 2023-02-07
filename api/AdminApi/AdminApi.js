2809a143ba9dcb232f94dd84587adc18d3d0c83c
iam5abhi <support@codesoftic.com>
2023-02-07 11:15:21 +0530

    dtaa

--- api/AdminApi/AdminApi.js
+++ api/AdminApi/AdminApi.js
@@ -14,12 +14,12 @@ adminrouter.use(isAuthenticated)
 adminrouter.route("/change-password").patch(admin.UpdatePassword)
 
 
-//Category Handler
-exports.AddCategory=FactorHandler.Add(Category)
-exports.getAllCategory=FactorHandler.getAll(Category)
-exports.EditCategory=FactorHandler.updateOne(Category)
-exports.UpdateStatus=FactorHandler.updateOne(Category)
-exports.getCategory=FactorHandler.getOne(Category)
+//Category
+adminrouter.route('/add-category').post(admin.AddCategory)
+adminrouter.route('/get-all-category').get(admin.getAllCategory)
+adminrouter.route('/edit-category/:id').patch(admin.EditCategory)
+adminrouter.route('/status/:id').patch(admin.UpdateStatus)
+adminrouter.route('/get-category/:id').get(admin.getCategory)
 
 
 
